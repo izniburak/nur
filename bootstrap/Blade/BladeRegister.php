73b10e532648e7d29c52243e0ae04a04e03a6bd4
İzni Burak Demirtaş <izniburak@outlook.com>
2016-11-01 02:56:33 +0300

    Updated and added new method for use css, js, use and namespaces directives in BladeTemplate.

--- bootstrap/Blade/BladeRegister.php
+++ bootstrap/Blade/BladeRegister.php
@@ -61,6 +61,8 @@ class BladeRegister
         $this->registerViewFinder();
 
         $this->instance = $this->registerFactory();
+
+        $this->registerDirectives($this->container['blade.compiler']);
     }
 
     public function view()
@@ -187,5 +189,50 @@ class BladeRegister
     public function getCompiler()
     {
         return $this->container['blade.compiler'];
+    }
+
+
+    /**
+     * Add extra directives to the blade templating compiler.
+     *
+     * @param BladeCompiler $blade The compiler to extend
+     *
+     * @return void
+     */
+    public function registerDirectives(BladeCompiler $blade)
+    {
+        $keywords = [
+            "namespace",
+            "use",
+        ];
+        foreach ($keywords as $keyword) {
+            $blade->directive($keyword, function ($parameter) use ($keyword) {
+                $parameter = trim($parameter, "()");
+                return "<?php {$keyword} {$parameter} ?>";
+            });
+        }
+        $assetify = function ($file, $type) {
+            $file = trim($file, "()");
+            if (in_array(substr($file, 0, 1), ["'", '"'], true)) {
+                $file = trim($file, "'\"");
+            } else {
+                return "{{ {$file} }}";
+            }
+            if (substr($file, 0, 1) !== "/") {
+                $file = "/{$type}/{$file}";
+            }
+            if (substr($file, (strlen($type) + 1) * -1) !== ".{$type}") {
+                $file .= ".{$type}";
+            }
+            return $file;
+        };
+        $blade->directive("css", function ($parameter) use ($assetify) {
+            $file = $assetify($parameter, "css");
+            return '<link rel="stylesheet" type="text/css" href="'.$file.'"/>';
+        });
+        $blade->directive("js", function ($parameter) use ($assetify) {
+            $file = $assetify($parameter, "js");
+            return '<script type="text/javascript" src="'.$file.'"></script>';
+        });
     }
 }
