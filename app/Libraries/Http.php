698aed2e8566b7228866f702c4d336c39f470ffa
İzni Burak Demirtaş <izniburak@outlook.com>
2016-11-01 03:03:01 +0300

    Added new global libraries for framework.

---
+++ app/Libraries/Http.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Mubu - A simple framework for PHP Developers
+ *
+ * @package  Mubu
+ * @author   İzni Burak Demirtaş <izniburak@gmail.com>
+ * @web      <http://burakdemirtas.org>
+ */
+
+use Mubu\Http\Http as HttpProvider;
+
+class Http extends HttpProvider
+{
+
+}
