8025ca0dc1cf1efe7fd9f28e8bb9396f687df9b1
İzni Burak Demirtaş <izniburak@outlook.com>
2017-02-21 17:29:26 +0300

    Update files.

--- index.php
+++ index.php
@@ -11,4 +11,4 @@ header('content-type: text/html; charset=utf-8');
 
 # FRAMEWORK START
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/bootstrap/bootstrap.php';
+require_once __DIR__ . '/vendor/izniburak/nur-core/src/bootstrap.php';
