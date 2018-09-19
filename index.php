1c255205a7a60b796b15462eecc8c80c818838dc
İzni Burak Demirtaş <izniburak@outlook.com>
2018-09-20 00:17:44 +0300

    Updated index.php and .env.example file

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 /**
-* nur - a simple framework for PHP Developers
-*
-* @author   izni burak demirtaş (@izniburak) <izniburak@gmail.com>
-* @web      <http://burakdemirtas.org>
-* @url      <https://github.com/izniburak/nur>
-* @license  The MIT License (MIT) - <http://opensource.org/licenses/MIT>
-*/
-header('content-type: text/html; charset=utf-8');
+ * nur - a simple framework for PHP Developers
+ *
+ * @author   izni burak demirtaş (@izniburak) <izniburak@gmail.com>
+ * @web      <http://burakdemirtas.org>
+ * @url      <https://github.com/izniburak/nur>
+ * @license  The MIT License (MIT) - <http://opensource.org/licenses/MIT>
+ */
 
 # FRAMEWORK START
 require_once __DIR__ . '/vendor/autoload.php';
