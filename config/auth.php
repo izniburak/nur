1f3046761385a5212afa352f7a91dbe21468b6b5
İzni Burak Demirtaş <izniburak@outlook.com>
2019-07-29 23:09:45 +0300

    Added JWT and Basic Auth configurations.

--- config/auth.php
+++ config/auth.php
@@ -21,4 +21,50 @@ return [
     // 'table'       => 'users',
     // 'primary_key' => 'id',
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | JWT
+    |--------------------------------------------------------------------------
+    |
+    | Json Web Token configurations
+    |
+    */
+
+    'jwt' => [
+        // JWT Authentication Secret
+        'secret' => env('JWT_SECRET'),
+
+        // JWT time to live (in minutes)
+        'ttl' => env('JWT_TTL', 60),
+
+        // JWT hashing algorithm
+        'alg' => env('JWT_ALGO', 'HS256'),
+
+        // Leeway
+        'leeway' => env('JWT_LEEWAY', 0),
+    ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Basic Authentication
+    |--------------------------------------------------------------------------
+    |
+    | Basic Authentication configurations.
+    |
+    | Supported: "default", "database"
+    | If you select "database", Auth configuration at above will be used
+    | for database connection.
+    |
+    */
+
+    'basic' => [
+        'credentials' => ['email', 'password'],
+        'driver'      => 'database',
+
+        // 'credentials' => [env('BASIC_AUTH_USER'), env('BASIC_AUTH_PASS')],
+        // 'driver'      => 'default',
+    ],
+
 ];
