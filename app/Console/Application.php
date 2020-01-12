74c8347d5c02dd2dfb76d7b739c5ddd226f8ad30
İzni Burak Demirtaş <izniburak@outlook.com>
2020-01-13 00:30:17 +0300

    Added custom Console command support.

---
+++ app/Console/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console;
+
+use Nur\Console\Application as ConsoleApplication;
+
+class Application extends ConsoleApplication
+{
+    /**
+     * The commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+
+    ];
+}
