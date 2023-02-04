c5beb4373f1613078d0aa1f88850822c7092e98e
İzni Burak Demirtaş <izniburak@outlook.com>
2023-02-04 17:10:15 +0300

    (chore): services configs moved into app configs

--- config/app.php
+++ config/app.php
@@ -115,4 +115,58 @@ return [
 
     'https' => env('HTTPS', false),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+
+        /*
+        * Nur Framework Service Providers...
+        */
+        Nur\Providers\Auth::class,
+        Nur\Providers\Blade::class,
+        // Nur\Providers\Cache::class,
+        Nur\Providers\Database::class,
+        Nur\Providers\Hash::class,
+        Nur\Providers\Mail::class,
+        // Nur\Providers\Translation::class,
+
+        /*
+        * Application Service Providers...
+        */
+        App\Providers\AppServiceProvider::class,
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+
+    'aliases' => [
+
+        'Auth'        => Nur\Facades\Auth::class,
+        // 'Cache'    => Nur\Facades\Cache::class,
+        'Config'      => Nur\Facades\Config::class,
+        'DB'          => Nur\Facades\DB::class,
+        'Hash'        => Nur\Facades\Hash::class,
+        'Jwt'         => Nur\Facades\Jwt::class,
+        'Mail'        => Nur\Facades\Mail::class,
+
+    ],
+
 ];
